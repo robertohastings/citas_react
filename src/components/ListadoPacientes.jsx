cbbc98a5ae676c9b630e1370a7141f576c3d4a49
Roberto Vázquez Hasting <robertovazquezhasting@MacBook-Air-de-Roberto.local>
2022-07-13 06:51:02 -0500

    first commit

---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,36 @@
+import Paciente from "./paciente"
+
+const ListadoPacientes = ({pacientes}) => {
+    
+  return (
+    <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-auto">
+
+      {pacientes && pacientes.length ? (
+        <>
+          <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Administa tus {''}
+            <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
+          </p>
+
+          { pacientes.map( paciente => (
+            <Paciente
+              key = {paciente.id}
+              paciente = {paciente}
+            />
+          ))}                
+        </>
+      ) : (
+        <>
+          <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Comienza agregando pacientes {''}
+            <span className="text-indigo-600 font-bold">y aparecerán en este lugar</span>
+          </p>        
+        </>
+      )}      
+    </div>
+  )
+}
+
+export default ListadoPacientes
