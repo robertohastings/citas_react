4f55026e1b387580fe7c19aaf8fa025eb52346db
robertohastings <rob.hst@gmail.com>
2022-07-15 16:28:47 -0500

    Boton eliminar
    Se agregó la funcionalidad del botón eliminar

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,6 +1,6 @@
 import Paciente from "./paciente"
 
-const ListadoPacientes = ({pacientes, setPaciente}) => {
+const ListadoPacientes = ({pacientes, setPaciente, eliminarPaciente}) => {
   
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-auto">
@@ -18,6 +18,7 @@ const ListadoPacientes = ({pacientes, setPaciente}) => {
               key = {paciente.id}
               paciente = {paciente}
               setPaciente = {setPaciente}
+              eliminarPaciente = {eliminarPaciente}
             />
           ))}                
         </>
--- src/components/paciente.jsx
+++ src/components/paciente.jsx
@@ -1,6 +1,14 @@
-const paciente = ({paciente, setPaciente}) => {
+const paciente = ({paciente, setPaciente, eliminarPaciente}) => {    
 
-    const { nombre, propietario, email, fecha, sintomas} = paciente
+    const { nombre, propietario, email, fecha, sintomas, id} = paciente
+
+    const handleEliminar = () =>{
+        const respuesta = confirm('Deseas eliminar este paciente')
+        if(respuesta){
+            eliminarPaciente(id)
+        }
+    }
+
   return (
     <div className="mx-5 my-5 bg-white shadow-md px-5 py-10 rounded-xl">
         <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
@@ -28,7 +36,9 @@ const paciente = ({paciente, setPaciente}) => {
             </button>
             <button 
                 type="button" 
-                className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg">Eliminar                
+                className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
+                onClick={handleEliminar}
+                >Eliminar                
             </button>            
         </div>     
     </div>
