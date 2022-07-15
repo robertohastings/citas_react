f5443bc0d514b4cccf0514574366f65f9e1acfff
robertohastings <rob.hst@gmail.com>
2022-07-15 10:23:24 -0500

    Update paciente.jsx
    Update className in pacient buttons

--- src/components/paciente.jsx
+++ src/components/paciente.jsx
@@ -20,12 +20,12 @@ const paciente = ({paciente}) => {
 
         <div className="flex justify-between mt-10">
             <button 
-                type="button">Editar
-                className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+                type="button" 
+                className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg">Editar                
             </button>
             <button 
-                type="button">Eliminar
-                className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
+                type="button" 
+                className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg">Eliminar                
             </button>            
         </div>     
     </div>
